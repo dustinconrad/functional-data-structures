ddb9f9ffc4529df8e6f80c726fa387aabda78501
Dustin Conrad <dustinaconrad@gmail.com>
2013-07-26 08:30:08 -0400

    minor edits

--- src/functional_data_structures/bst.clj
+++ src/functional_data_structures/bst.clj
@@ -8,20 +8,14 @@
 (defn member? [tree x]
   (cond
     (empty? tree) false
-    (< x (:value tree)) (member? (:left tree) x)
-    (> x (:value tree)) (member? (:right tree) x)
+    (< x (:value tree)) (recur (:left tree) x)
+    (> x (:value tree)) (recur (:right tree) x)
     :equal true))
 
 (defn insert [tree x]
   (cond
-    (empty? tree) (make-tree nil x nil)
+    (empty? tree) (make-tree x)
     (< x (:value tree)) (make-tree (insert (:left tree) x) (:value tree) (:right tree))
     (> x (:value tree)) (make-tree (:left tree) (:value tree) (insert (:right tree) x))
     :equal tree
     ))
-
-(defprotocol Set
-  (is-empty? [this])
-  (member? [this value])
-  (insert [this value]))
-
