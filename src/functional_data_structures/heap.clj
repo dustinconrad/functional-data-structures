f1791584195d264fbf7eefcc91279bfcabe73423
Dustin Conrad <dustinaconrad@gmail.com>
2013-08-14 20:54:01 -0400

    find/delete0min

--- src/functional_data_structures/heap.clj
+++ src/functional_data_structures/heap.clj
@@ -18,8 +18,18 @@
 (defn merge-heap [h1 h2]
   (cond
     (empty? h1) h2
-    (empty? h2) h2
+    (empty? h2) h1
     (<= (:value h1) (:value h2)) (make-heap (:value h1) (:left h1) (merge-heap (:right h1) h2))
     :default (make-heap (:value h2) (:left h2) (merge-heap (:right h2) h1))
     ))
 
+(defn insert [heap x]
+  (merge-heap heap (make-heap x)))
+
+(defn find-min [heap]
+  {:pre [((complement empty?) heap)]}
+  (:value heap))
+
+(defn delete-min [heap]
+  {:pre [((complement empty?) heap)]}
+  (merge-heap (:left heap) (:right heap)))
