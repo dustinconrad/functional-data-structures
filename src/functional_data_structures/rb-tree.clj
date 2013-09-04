d70deb2105de8cb901b8731d7c76d69bdf7cc2d5
Dustin Conrad <dustinaconrad@gmail.com>
2013-09-03 23:31:58 -0400

    first pass at insert for red-black tree

--- src/functional_data_structures/rb-tree.clj
+++ src/functional_data_structures/rb-tree.clj
@@ -1,7 +1,7 @@
 (ns functional-data-structures.rb-tree)
 
 (defn make-rb-tree
-  ([value] (make-tree :red nil value nil))
+  ([value] (make-rb-tree :R nil value nil))
   ([color left value right]
     {:color color :left left :value value :right right}))
 
@@ -11,3 +11,13 @@
     (< x y) (recur a x)
     (> x y) (recur b x)
     :equal true))
+
+(defn insert [s-prime x-prime]
+  (let [ins (fn [{color :color a :left y :value b :right :as s} x]
+              (cond
+                (empty? s) (make-rb-tree x)
+                (< x y) (balance color (ins a x) y b)
+                (>x y) (balance color a y (ins b x))
+                :default s))
+        {a-prime :left y-prime :value b-prime :right} (ins s-prime x-prime)]
+    (make-rb-tree :B a-prime y-prime b-prime)))
