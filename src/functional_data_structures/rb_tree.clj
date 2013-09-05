5a88cfc8ad2e16a3cea0cd839f86d8259f9e86c4
Dustin Conrad <dustinaconrad@gmail.com>
2013-09-05 18:25:02 -0400

    update to balance

--- src/functional_data_structures/rb_tree.clj
+++ src/functional_data_structures/rb_tree.clj
@@ -14,21 +14,22 @@
     (> x y) (recur b x)
     :equal true))
 
-(defn balance [tree]
-  (core/match [tree]
-    [(:or {:color :B , :left {:color :R , :left {:color :R , :left a, :value x, :right b},:value y, :right c}, :value z, :right d}
-          {:color :B , :left {:color :R , :left a, :value x, :right {:color :R , :left b, :value y, :right c}}, :value z, :right d}
-          {:color :B , :left a, :value x, :right {:color :R , :left {:color :R , :left b, :value y, :right c} :value z, :right d}}
-          {:color :B , :left a, :value x, :right {:color :R , :left b, :value y, :right {:color :R , :left c, :value z, :right d}}})]
+(defn balance [color left value right]
+  (core/match [[color left value right]]
+    [(:or [:B {:color :R , :left {:color :R , :left a, :value x, :right b},:value y, :right c} z d]
+          [:B {:color :R , :left a, :value x, :right {:color :R , :left b, :value y, :right c}} z d]
+          [:B a x {:color :R , :left {:color :R , :left b, :value y, :right c} :value z, :right d}]
+          [:B a x {:color :R , :left b, :value y, :right {:color :R , :left c, :value z, :right d}}])]
     (make-rb-tree :R (make-rb-tree :B a x b) y (make-rb-tree :B c z d))
-    :else tree))
+    :else (make-rb-tree color left value right)))
 
 (defn insert [s-prime x-prime]
-  (let [ins (fn f [{color :color a :left y :value b :right :as s} x]
+  (let [ins (fn insrt [{color :color a :left y :value b :right :as s} x]
               (cond
                 (empty? s) (make-rb-tree x)
-                (< x y) (balance (make-rb-tree color (f a x) y b))
-                (> x y) (balance (make-rb-tree color a y (f b x)))
+                (< x y) (balance color (insrt a x) y b)
+                (> x y) (balance color a y (insrt b x))
                 :default s))
         {a-prime :left y-prime :value b-prime :right} (ins s-prime x-prime)]
     (make-rb-tree :B a-prime y-prime b-prime)))
+ 
