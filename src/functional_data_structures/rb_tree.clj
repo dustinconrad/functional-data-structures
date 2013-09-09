2934be6898e0534e2de500ead7a74d4991a16be2
Dustin Conrad <dustinaconrad@gmail.com>
2013-09-08 21:47:16 -0400

    lbalance and rbalance implemented

--- src/functional_data_structures/rb_tree.clj
+++ src/functional_data_structures/rb_tree.clj
@@ -23,12 +23,26 @@
     (make-rb-tree :R (make-rb-tree :B a x b) y (make-rb-tree :B c z d))
     :else (make-rb-tree color left value right)))
 
+(defn lbalance [color left value right]
+  (core/match [[color left value right]]
+    [(:or [:B {:color :R , :left {:color :R , :left a, :value x, :right b},:value y, :right c} z d]
+          [:B {:color :R , :left a, :value x, :right {:color :R , :left b, :value y, :right c}} z d])]
+    (make-rb-tree :R (make-rb-tree :B a x b) y (make-rb-tree :B c z d))
+    :else (make-rb-tree color left value right)))
+
+(defn rbalance [color left value right]
+  (core/match [[color left value right]]
+    [(:or [:B a x {:color :R , :left {:color :R , :left b, :value y, :right c} :value z, :right d}]
+          [:B a x {:color :R , :left b, :value y, :right {:color :R , :left c, :value z, :right d}}])]
+    (make-rb-tree :R (make-rb-tree :B a x b) y (make-rb-tree :B c z d))
+    :else (make-rb-tree color left value right)))
+
 (defn insert [s x]
   (let [ins (fn ins [{color :color a :left y :value b :right :as s} x]
               (cond
                 (empty? s) (make-rb-tree x)
-                (< x y) (balance color (ins a x) y b)
-                (> x y) (balance color a y (ins b x))
+                (< x y) (lbalance color (ins a x) y b)
+                (> x y) (rbalance color a y (ins b x))
                 :default s))
         {a-prime :left y-prime :value b-prime :right} (ins s x)]
     (make-rb-tree :B a-prime y-prime b-prime)))
