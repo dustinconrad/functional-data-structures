27de81bc308602db4b88885ba8a80ee41ec25e5b
Dustin Conrad <dustinaconrad@gmail.com>
2013-09-09 20:24:33 -0400

    take implemented

--- src/functional_data_structures/stream.clj
+++ src/functional_data_structures/stream.clj
@@ -1,7 +1,14 @@
-(ns functional-data-structures.stream)
+(ns functional-data-structures.stream
+  (:refer-clojure :exclude [take]))
 
 (defn ++ [[x & s :as xs] t]
   (if (empty? xs) 
     t
     (lazy-seq (cons x (lazy-seq (++ s t))))
     ))
+
+(defn take [n [x & s :as xs]]
+  (cond
+    (zero? n) (lazy-seq nil)
+    (nil? xs) (lazy-seq nil)
+    :else (lazy-seq (cons x (lazy-seq (take (dec n) s))))))
--- test/functional_data_structures/stream_test.clj
+++ test/functional_data_structures/stream_test.clj
@@ -1,6 +1,6 @@
 (ns functional-data-structures.stream-test
   (:require [clojure.test :refer :all ]
-            [functional-data-structures.stream :refer :all ]))
+            [functional-data-structures.stream :refer :all :exclude [take]]))
 
 (defmacro is-lazy? [x] `(is (instance? clojure.lang.LazySeq ~x)))
 
@@ -19,4 +19,19 @@
             (is (is-lazy? together))
             (is (not (realized? together)))
             (is (= (concat l r) (++ l r))))))
+      doall)))
+
+(deftest test-take
+  (testing "random takes"
+    (->>
+      (for [i (range 1 11)]
+        [(rand-int 25) (take (rand-int 25) (repeatedly #(rand-int 25)))])
+      (map
+        (fn [[n c]]
+          (let [coll (doall c)
+                taken (functional-data-structures.stream/take n coll)]
+            (is (realized? c))
+            (is (is-lazy? taken))
+            (is (not (realized? taken)))
+            (is (= (take n coll) taken)))))
       doall)))
