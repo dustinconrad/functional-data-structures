97e38357467744d0f4389c45552c0e5cad25e9e5
Dustin Conrad <dustinaconrad@gmail.com>
2013-09-23 15:02:26 -0400

    add Map protocol

--- src/functional_data_structures/set.clj
+++ src/functional_data_structures/set.clj
@@ -1,10 +1,14 @@
 (ns functional-data-structures.set)
 
 (defprotocol Set
-  (is-empty? [this])
-  (is-member? [this value])
-  (insert [this value]))
+  (is-empty? [s])
+  (is-member? [s value])
+  (insert [s value]))
 
 (defprotocol SmartSet
-  (smart-insert [this value])
-  (smart-member? [this value]))
+  (smart-insert [ss value])
+  (smart-member? [ss value]))
+
+(defprotocol Map
+  (bind [m k v])
+  (lookup [m k]))
